50ba7004f039597767551f9d0edc2c561d7d4f9c
chuujokohei <chuujokohei@yahoo.co.jp>
2026-06-13 08:13:29 +0900

    Initial Commit

---
+++ MeetingTranscriber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetingTranscriber
+//
+//  Created by 中條航平 on 2026/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetingTranscriber/MeetingTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingTranscriberApp.swift
+//  MeetingTranscriber
+//
+//  Created by 中條航平 on 2026/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MeetingTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
